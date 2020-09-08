ae37df3ee791310acb7b20786b7c2053ca01bf12
Miquel Mariño Espinosa <miquel@MacBook-Air-de-Miquel.local>
2020-09-08 16:56:01 +0200

    update symfony dependencies

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,9 +3,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Php\Fpm\AppKernel;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Debug\Debug;
 
-Debug::enable();
 $kernel = new AppKernel('dev', true);
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
